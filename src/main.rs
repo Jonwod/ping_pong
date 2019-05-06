50a757fe8f53d3375033a2d19b5266a73e9958a3
Jon <jonathan.m.wood95@gmail.com>
2019-05-06 13:35:46 +0100

    Enabled listening to hear messages of any length (and print them to the console as each character is read)

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
 use std::io::prelude::*;
+use std::io::Write;
 use std::net::{TcpListener, TcpStream};
 use std::str;
 
 
 fn pong(stream: &mut TcpStream) {
-    let mut my_buff_buff : &mut [u8] = &mut[0, 0];
-    stream.read(my_buff_buff).unwrap();
-   
-    let s = str::from_utf8(my_buff_buff);
+    let mut buff = &mut [0 as u8];
 
-    println!("Got {}", s.unwrap());
+    while stream.read(buff).unwrap() != 0 {
+        print!("{}", str::from_utf8(buff).unwrap());
+        std::io::stdout().flush().unwrap();
+    }
 }
 
 
 fn main() -> std::io::Result<()>{
-
     let listener = TcpListener::bind("127.0.0.1:5780")?;
 
-    let mut stream = TcpStream::connect("51.7.187.57:5780")?;
+    let mut stream = TcpStream::connect("127.0.0.1:5780")?;
 
-    stream.write(&[b'h', b'a'])?;
+    let my_str : &str = "Hello there";
 
-    // accept connections and process them serially
+    stream.write(&my_str.as_bytes())?;
+
     for stream in listener.incoming() {
         pong(&mut stream?);
     }
